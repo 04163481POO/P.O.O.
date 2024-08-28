e6087466c870cde50ec29779ae0d70472e4e741a
04163481POO <041634812@protonmail.com>
2024-08-28 12:51:17 +0000

    #

---
+++ Tarefa1.java
@@ -0,0 +1,11 @@
+class BALANÇOtrimestral {
+    public static void main(String[] args) {
+        int GASTOjaneiro = 15000;
+        int GASTOfevereiro = 23000;
+        int GASTOmarço = 17000;
+        int GASTOtrimestre = GASTOjaneiro + GASTOfevereiro + GASTOmarço;
+        
+        System.out.println(GASTOtrimestre);
+    }
+
+}
---
+++ TesteIdade.java
@@ -0,0 +1,16 @@
+class TESTEidade {
+    public static void main(String[] args) {
+        int Idade;
+        Idade = 15;
+        int IDADEnoANOqueVEM = Idade + 1;
+        int DOBROidade = Idade * 2;
+        double MEIAidade = Idade / 2.0;
+        boolean MAIORdeIDADE = Idade >= 18;
+
+        System.out.println(Idade);
+        System.out.println(IDADEnoANOqueVEM);
+        System.out.println(DOBROidade);
+        System.out.println(MEIAidade);
+        System.out.println(MAIORdeIDADE);
+        }
+}   
